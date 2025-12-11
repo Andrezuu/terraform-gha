b8dd87b3bc398c1df134b5b42cb157b71ab65454
andrezu <sanchezandres8203@gmail.com>
2025-12-11 08:49:24 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,81 @@
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["andres-vpc-vpc"]
+  }
+}
+
+data "aws_subnets" "selected" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-*-x86_64"]
+  }
+
+  owners = ["amazon"]
+}
+
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-datasource-sg-2"
+  vpc_id      = data.aws_vpc.selected.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-datasource-sg-2"
+  }
+}
+
+# 4. Crear EC2 usando esos data sources
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.small"
+  subnet_id     = data.aws_subnets.selected.ids[0]
+  associate_public_ip_address = true
+
+  user_data = <<EOF
+#!/bin/bash
+sudo yum install httpd -y
+echo "Hola Mundo desde Terraform con Data Sources" > /var/www/html/index.html
+sudo systemctl enable --now httpd
+EOF
+
+  tags = {
+    Name = "andres-web-datasource"
+  }
+}
